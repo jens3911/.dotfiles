ada9c6e6ba3803d9ac568cbaecce2c53d319422b
jens3911 <91621087+jens3911@users.noreply.github.com>
2022-06-10 16:44:55 +0200

    Update configs.lua

--- nvim/.config/nvim/lua/user/lsp/configs.lua
+++ nvim/.config/nvim/lua/user/lsp/configs.lua
@@ -15,7 +15,7 @@ for _, server in pairs(servers) do
 	local opts = {
 		on_attach = require("user.lsp.handlers").on_attach,
 		capabilities = require("user.lsp.handlers").capabilities,
-    root_dir = vim.loop.cwd -- adds lsp without tsconfig,...
+    -- root_dir = vim.loop.cwd -- adds lsp without tsconfig,...
 	}
 	local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server)
 	if has_custom_opts then
